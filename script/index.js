4540e5e21f1e226910f8c763dc8694297d90aedd
Aliaksandr Hrychonak <alexsanderkorn90@gmail.com>
2021-05-13 13:22:38 +0300

    add popup)opened

--- script/index.js
+++ script/index.js
@@ -1,5 +1,11 @@
-let openPopup = document.querySelector('.profile__button-edit');
 
-openPopup.addEventListener('click', function () {
-  openPopup.classList.add('popup_opened')
+let buttonEdit = document.querySelector('.profile__button-edit');
+let popupDefoult = document.querySelector('.popup');
+
+buttonEdit.addEventListener('click', () => {
+  document.querySelector('.popup').classList.toggle('popup_opened');
+});
+
+document.querySelector('.popup__button-close').addEventListener('click', () => {
+  document.querySelector('.popup').classList.remove('popup_opened');
 });
