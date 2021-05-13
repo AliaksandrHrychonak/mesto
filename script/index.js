4efaa4d901c14b75b9e43a41af490328fb454940
Aliaksandr Hrychonak <alexsanderkorn90@gmail.com>
2021-05-14 01:59:07 +0300

    add popup__form,popup buttons , footer(padding), media responsive.

--- script/index.js
+++ script/index.js
@@ -1,7 +1,12 @@
-
+// popup
 let buttonEdit = document.querySelector('.profile__button-edit');
 let buttonClosePopup = document.querySelector('.popup__button-close');
 let popupDefoult = document.querySelector('.popup');
+// formElement
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_value_name');
+let jobInput = document.querySelector('.popup__input_value_job');
+
 
 popupDefoult.classList.remove('popup_opened');
 
@@ -12,3 +17,22 @@ buttonEdit.addEventListener('click', () => {
 buttonClosePopup.addEventListener('click', () => {
   popupDefoult.classList.remove('popup_opened');
 });
+
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
